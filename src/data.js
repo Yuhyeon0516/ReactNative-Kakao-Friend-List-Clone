92837acc7baf8b2e60b1e81ee00b2f51c07eb829
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-20 14:39:14 +0900

    Run "npx create-expo-app" and friend data create

---
+++ src/data.js
@@ -0,0 +1,73 @@
+export const myProfile = {
+  uri: "https://png.pngtree.com/thumb_back/fh260/back_pic/03/54/06/62579986dba62df.jpg",
+  name: "Yuhyeon",
+  introduction: "inspiring day",
+};
+
+export const friendProfiles = [
+  {
+    uri: "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__480.jpg",
+    name: "minho",
+    introduction: "Minho Kim",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2018/04/05/14/09/sunflowers-3292932__480.jpg",
+    name: "jiyeon",
+    introduction: "a man of heart",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616__480.jpg",
+    name: "yeji",
+    introduction: "life is real",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2017/12/15/13/51/polynesia-3021072__480.jpg",
+    name: "soyeon",
+    introduction: "24/7",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2018/05/30/15/39/thunderstorm-3441687__480.jpg",
+    name: "minkyong",
+    introduction: "",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2017/08/10/02/05/tiles-shapes-2617112_1280.jpg",
+    name: "juyong",
+    introduction: "",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2016/03/09/09/30/woman-1245817__480.jpg",
+    name: "jihye",
+    introduction: "Do the Next things",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2018/01/15/22/25/gingerbread-men-3084961__480.jpg",
+    name: "sungen",
+    introduction: "hahaha",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2016/02/09/16/35/night-1189929__480.jpg",
+    name: "dahee",
+    introduction: "i can doing",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2018/09/24/08/52/mountains-3699372__480.jpg",
+    name: "hyeonso",
+    introduction: "fighting",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2019/03/25/22/52/poodle-4081526__480.jpg",
+    name: "sein",
+    introduction: "sad",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2018/09/29/23/09/watermelon-3712515__480.jpg",
+    name: "yurim",
+    introduction: "enjoy",
+  },
+  {
+    uri: "https://cdn.pixabay.com/photo/2017/08/30/17/12/waffle-hearts-2697904__480.jpg",
+    name: "juhyeon",
+    introduction: "~off~",
+  },
+];
