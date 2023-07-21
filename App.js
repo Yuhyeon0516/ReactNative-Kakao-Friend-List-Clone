9b10bb2780bd412f2147dbfebebcbf658c1d8303
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-21 15:55:19 +0900

    create Margin.js, MyProfile.js

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Header from "./src/components/Header";
+import MyProfile from "./src/components/MyProfile";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { myProfile } from "./src/data";
+import Margin from "./src/components/Margin";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <Header />
+        <Margin height={10} />
+        <MyProfile uri={myProfile.uri} name={myProfile.name} introduction={myProfile.introduction} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
---
+++ src/components/MyProfile.js
@@ -0,0 +1,18 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import Margin from "./Margin";
+
+const MyProfile = ({ uri, name, introduction }) => {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <Image source={{ uri: uri }} style={{ width: 50, height: 50, borderRadius: 20 }} />
+      <View style={{ justifyContent: "center", marginLeft: 10 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 16 }}>{name}</Text>
+        <Margin height={6} />
+        <Text style={{ fontSize: 12, color: "grey" }}>{introduction}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default MyProfile;
