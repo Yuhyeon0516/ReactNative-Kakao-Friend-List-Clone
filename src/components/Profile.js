99227a4fa3b517de2047440992724da814f858c0
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-21 18:05:07 +0900

    Using Styled-Components to refactoring Profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,27 +1,51 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
 import Margin from "./Margin";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex-direction: row;
+  background-color: white;
+`;
+
+const ProfileImage = styled.Image`
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
+  border-radius: ${(props) => props.size * 0.4}px;
+`;
+
+const TextContainer = styled.View`
+  justify-content: center;
+  margin-left: 10px;
+`;
+
+const NameText = styled.Text`
+  font-weight: ${(props) => (props.isMe ? "bold" : "normal")};
+  font-size: ${(props) => (props.isMe ? 16 : 15)}px;
+`;
+
+const IntroductionText = styled.Text`
+  font-size: ${(props) => (props.isMe ? 12 : 11)}px;
+  color: grey;
+`;
 
 const Profile = ({ uri, name, introduction, isMe }) => {
   const size = isMe ? 50 : 40;
-  const fontWeight = isMe ? "bold" : undefined;
-  const nameFontSize = isMe ? 16 : 15;
-  const introFontSize = isMe ? 12 : 11;
   const margin = isMe ? 6 : 2;
 
   return (
-    <View style={{ flexDirection: "row", backgroundColor: "white" }}>
-      <Image source={{ uri: uri }} style={{ width: size, height: size, borderRadius: size * 0.4 }} />
-      <View style={{ justifyContent: "center", marginLeft: 10 }}>
-        <Text style={{ fontWeight: fontWeight, fontSize: nameFontSize }}>{name}</Text>
+    <Container>
+      <ProfileImage source={{ uri: uri }} size={size} />
+      <TextContainer>
+        <NameText isMe={isMe}>{name}</NameText>
         {!!introduction && (
           <View>
             <Margin height={margin} />
-            <Text style={{ fontSize: introFontSize, color: "grey" }}>{introduction}</Text>
+            <IntroductionText isMe={isMe}>{introduction}</IntroductionText>
           </View>
         )}
-      </View>
-    </View>
+      </TextContainer>
+    </Container>
   );
 };
 
